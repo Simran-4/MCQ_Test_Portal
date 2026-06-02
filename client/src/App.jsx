c39520a149a5eb42349633cf7c11117bf7101931
Ved <vedantsambhajitambe@Veds-Mac.local>
2026-06-02 14:46:09 +0530

    Fixed translation widget

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
@@ -8,56 +8,93 @@ import ViewQuestions from "./pages/ViewQuestions";
 import Test from "./pages/Test";
 import Register from "./pages/register";
 import ViewResults from "./pages/ViewResults";
-import AdminSettings
-from "./pages/AdminSettings";
+import AdminSettings from "./pages/AdminSettings";
 
 function App() {
 
+  useEffect(() => {
+
+    const addScript = document.createElement("script");
+
+    addScript.src =
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+
+    addScript.async = true;
+
+    document.body.appendChild(addScript);
+
+    window.googleTranslateElementInit = () => {
+
+      new window.google.translate.TranslateElement(
+        {
+          pageLanguage: "en",
+          includedLanguages: "en,hi,mr",
+          layout:
+            window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+        },
+        "google_translate_element"
+      );
+    };
+
+  }, []);
+
   return (
 
-    <BrowserRouter>
+    <>
 
-      <Routes>
+      <div id="google_translate_element"></div>
 
-        <Route path="/" element={<Login />} />
+      <BrowserRouter>
 
-        <Route path="/register" element={<Register />} />
+        <Routes>
 
-        <Route path="/results" element={<ViewResults />} />
+          <Route path="/" element={<Login />} />
 
-        <Route
-  path="/view-results"
-  element={<ViewResults />}
-/>
+          <Route
+            path="/register"
+            element={<Register />}
+          />
 
-        <Route
-  path="/settings"
-  element={<AdminSettings />}
-/>
+          <Route
+            path="/results"
+            element={<ViewResults />}
+          />
 
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
+          <Route
+            path="/view-results"
+            element={<ViewResults />}
+          />
 
-        <Route
-          path="/add-question"
-          element={<AddQuestion />}
-        />
+          <Route
+            path="/settings"
+            element={<AdminSettings />}
+          />
 
-        <Route
-          path="/view-questions"
-          element={<ViewQuestions />}
-        />
+          <Route
+            path="/dashboard"
+            element={<Dashboard />}
+          />
 
-        <Route
-          path="/test"
-          element={<Test />}
-        />
+          <Route
+            path="/add-question"
+            element={<AddQuestion />}
+          />
 
-      </Routes>
+          <Route
+            path="/view-questions"
+            element={<ViewQuestions />}
+          />
 
-    </BrowserRouter>
+          <Route
+            path="/test"
+            element={<Test />}
+          />
+
+        </Routes>
+
+      </BrowserRouter>
+
+    </>
 
   );
 }
